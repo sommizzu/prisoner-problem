e9b80a3ec2db0013efb1d6fa6ae004597490ef8e
jihwan1229 <5718719@naver.com>
2024-01-15 18:15:27 +0900

    feat: Initalize box, prisoner in simulate_prisoner function

--- prisoner.py
+++ prisoner.py
@@ -1,1 +1,11 @@
-print('hello')
+
+import random
+
+def simulate_prisoner(num_prisoners: int=100, max_attempts: int=50):
+    boxes = {}
+
+    prisoners = list(range(1, num_prisoners + 1))
+    random.shuffle(prisoners)
+
+if __name__ == '__main__':
+    pass
